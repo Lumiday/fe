bfcd07def8ce22b8a47c591dfa7ebd783a5bfa05
June Seok Woo <40357804+suk3952111@users.noreply.github.com>
2025-06-21 01:19:22 +0900

    refactor: theme_dropdown 컴포넌트 하위 컴포넌트 제목의 with를 w-24에서 w-[100px]로 제대로 값 적용

--- app/_components/theme_dropdown/DropDownFont.tsx
+++ app/_components/theme_dropdown/DropDownFont.tsx
@@ -40,7 +40,7 @@ export default function DropDownFont({
 
   return (
     <div className="self-stretch inline-flex justify-start items-center gap-0.5">
-      <div className="w-24 h-10 px-1 py-1.5 flex justify-start items-center gap-2.5">
+      <div className="w-[100px] h-10 px-1 py-1.5 flex justify-start items-center gap-2.5">
         <div className="justify-start text-gray-900 text-base font-medium font-['Pretendard']">
           글꼴
         </div>
