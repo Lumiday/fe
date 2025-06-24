d3c66d22bac12bd779d5e31912f6aaaeb498fdea
hyun522 <jhj1004v@naver.com>
2025-06-24 21:49:55 +0900

    DESIGN : gallery dropdown 컴포넌트 작업중

---
+++ app/_components/gallery_dropdown/Gallery_Dropdown.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import CommonInput from '@/app/_components/common/CommonInput';
+import DropDownBig from '@/app/_components/common/DropDownBig';
+import Select_type from '@/app/_components/gallery_dropdown/Select_type';
+import { useState } from 'react';
+
+const patterns = [
+  { value: '스와이프', label: '스와이프' },
+  { value: '그리드', label: '그리드' },
+  { value: '작은 꽃', label: '작은 꽃' },
+];
+
+const Gallery_Dropdown = () => {
+  const [selectedPattern, setSelectedPattern] = useState('스와이프');
+
+  const handlePatternChange = (newPattern: string) => {
+    setSelectedPattern(newPattern);
+  };
+
+  return (
+    <DropDownBig name="갤러리" isSwitchVisible={false}>
+      {/* <CommonInput type="text" label="제목" id="title" inputWidth={21.6875} /> */}
+      {/* 갤러리 타입 */}
+      <Select_type
+        name="갤러리 타입"
+        patterns={patterns}
+        selectedPattern={selectedPattern}
+        setSelectedPattern={handlePatternChange}
+      />
+    </DropDownBig>
+  );
+};
+
+export default Gallery_Dropdown;
