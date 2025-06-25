32ab2d0c8f45a66492f7340566b95b6fda43ada1
hyun522 <jhj1004v@naver.com>
2025-06-26 00:07:31 +0900

    CHORE : type 수정 외

--- type/components.ts
+++ type/components.ts
@@ -58,6 +58,7 @@ export interface DropDownBigProps {
   checked?: boolean;
   setChecked?: (checked: boolean) => void;
   isSwitchVisible: boolean;
+  isPadding: boolean;
 }
 
 export interface CommonInputProps {
