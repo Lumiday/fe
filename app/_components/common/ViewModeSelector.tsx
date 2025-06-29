323f25f8e6564513a131b218e898ca544fad2fc9
June Seok Woo <40357804+suk3952111@users.noreply.github.com>
2025-06-29 23:42:16 +0900

    refactor: ViewModeSelector 트랜지션 추가

--- app/_components/common/ViewModeSelector.tsx
+++ app/_components/common/ViewModeSelector.tsx
@@ -17,7 +17,7 @@ export default function ViewModeSelector({
   patterns,
 }: ViewModeSelectorProps) {
   return (
-    <div className="flex items-center gap-[0.12rem] ">
+    <div className="flex items-center gap-[0.12rem]">
       <div className="w-[6.25rem] h-[2.5rem] px-[0.25rem] py-[0.4375rem]">
         <div className="text-LumiDayGray-1e1 text-1-500 font-['Pretendard']">
           {name}
@@ -27,9 +27,10 @@ export default function ViewModeSelector({
         {patterns?.map((pattern) => (
           <div
             key={pattern.value}
-            className={`h-[2.5rem] px-[0.625rem] py-[0.375rem] rounded-[0.3125rem] flex items-center 
+            className={`h-[2.5rem] px-[0.625rem] py-[0.375rem] rounded-[0.3125rem] hover:cursor-pointer flex items-center 
                 ${selectedPattern === pattern.value ? 'bg-white text-LumiDayGray-1e1 border border-LumiDayGray-1e1' : 'bg-LumiDayGray-f0f text-LumiDayGray-999 border border-LumiDayGray-f0f'} 
-                hover:text-LumiDayGray-1e1 hover:border hover:border-[#1e1e1e] ${pattern.className || ''}`}
+                hover:text-LumiDayGray-1e1 hover:border hover:border-[#1e1e1e] ${pattern.className || ''} 
+                transition-all duration-300 ease-in-out`}
             onClick={() => setSelectedPattern(pattern.value)}
           >
             <div>{pattern.label}</div>
