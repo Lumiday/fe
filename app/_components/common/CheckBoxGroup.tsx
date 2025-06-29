f4d1524dceafb49e240d887d467f3e2abd312b0b
June Seok Woo <40357804+suk3952111@users.noreply.github.com>
2025-06-29 23:41:55 +0900

    refactor: CheckBoxGroup 컴포넌트 클릭시 figma에 따른 이미지를 따르게 하기 위하여 리팩토링

--- app/_components/common/CheckBoxGroup.tsx
+++ app/_components/common/CheckBoxGroup.tsx
@@ -1,3 +1,5 @@
+import { CheckBoxWithLabel } from './CheckBoxWithLabel';
+
 interface CheckBoxItem {
   id: string;
   label: string;
@@ -17,26 +19,24 @@ const CheckBoxGroup = ({ title, items, onChange }: CheckBoxGroupProps) => {
       <p className="w-[6.25rem] py-[0.4375rem] px-[0.25rem] text-1-500 text-LumiDayGray-1e1 mr-[0.25rem]">
         {title}
       </p>
-      {items.map((item) => (
-        <div key={item.id} className="flex items-center h-[2.5rem]">
-          <div className="flex items-center mr-[0.62rem]">
-            <input
-              id={item.id}
-              type="checkbox"
-              value={item.value}
+      <div className="flex items-center justify-start gap-[0.625rem]">
+        {items.map((item) => (
+          <div key={item.id} className="flex items-center h-[2.5rem]">
+            <CheckBoxWithLabel
               checked={item.checked || false}
-              onChange={(e) => onChange(e, item.id)}
-              className="border h-full p-[1rem] border-[#999] rounded-[0.1875rem]"
+              onChange={() =>
+                onChange(
+                  {
+                    target: { checked: !item.checked },
+                  } as React.ChangeEvent<HTMLInputElement>,
+                  item.id
+                )
+              }
+              label={item.label}
             />
-            <label
-              htmlFor={item.id}
-              className="text-875-400 ml-[0.31rem] text-LumiDayGray-1e1"
-            >
-              {item.label}
-            </label>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
