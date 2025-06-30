09e070d44b84b070b72c4c64cd1f76bd4aa741f6
hyun522 <jhj1004v@naver.com>
2025-07-01 08:37:07 +0900

    사진업로드 작업중

--- app/_components/common/CheckBoxGroup.tsx
+++ app/_components/common/CheckBoxGroup.tsx
@@ -42,3 +42,47 @@ const CheckBoxGroup = ({ title, items, onChange }: CheckBoxGroupProps) => {
 };
 
 export default CheckBoxGroup;
+// interface CheckBoxItem {
+//   id: string;
+//   label: string;
+//   value: string;
+//   checked?: boolean;
+// }
+
+// interface CheckBoxGroupProps {
+//   title: string;
+//   items: CheckBoxItem[];
+//   onChange: (e: React.ChangeEvent<HTMLInputElement>, itemId: string) => void;
+// }
+
+// const CheckBoxGroup = ({ title, items, onChange }: CheckBoxGroupProps) => {
+//   return (
+//     <div className="flex items-center h-[2.5rem]">
+//       <p className="w-[6.25rem] py-[0.4375rem] px-[0.25rem] text-1-500 text-LumiDayGray-1e1 mr-[0.25rem]">
+//         {title}
+//       </p>
+//       {items.map((item) => (
+//         <div key={item.id} className="flex items-center h-[2.5rem]">
+//           <div className="flex items-center mr-[0.62rem]">
+//             <input
+//               id={item.id}
+//               type="checkbox"
+//               value={item.value}
+//               checked={item.checked || false}
+//               onChange={(e) => onChange(e, item.id)}
+//               className="border h-full p-[1rem] border-[#999] rounded-[0.1875rem]"
+//             />
+//             <label
+//               htmlFor={item.id}
+//               className="text-875-400 ml-[0.31rem] text-LumiDayGray-1e1"
+//             >
+//               {item.label}
+//             </label>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CheckBoxGroup;
