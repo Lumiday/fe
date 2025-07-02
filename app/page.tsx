40120b305de3aabe89e9d69d5b58f59a2a259763
hyun522 <jhj1004v@naver.com>
2025-07-02 22:22:43 +0900

    DESIGN :  layout 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import GalleryDropdown from '@/app/_components/gallery_dropdown/Gallery_Dropdown';
-
 export default function Home() {
-  return (
-    <>
-      <GalleryDropdown />
-    </>
-  );
+  return <>page</>;
 }
